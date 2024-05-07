f265521717a3464f24bce891f5a5a32fada8dac5
md_patrik <xd2.0.hun@gmail.com>
2024-05-07 15:53:38 +0200

    Update new 1.php

--- new 1.php
+++ new 1.php
@@ -5,7 +5,7 @@ $password = '';
 $db = 'users';
 
 $conn = new mysqli ($host,$user,$password,$db);
-if(conn->connect_error) {
+if($conn->connect_error) {
 	echo 'Sikeres kapcsolódás adatbázishoz!'}
 echo 'Sikeres db kapcsolódás!';
 ?>
