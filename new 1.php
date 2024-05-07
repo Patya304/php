21541182e1845dee1b8f4b4db709e2131af5540d
md_patrik <xd2.0.hun@gmail.com>
2024-05-07 15:50:04 +0200

    Add files via upload

---
+++ new 1.php
@@ -0,0 +1,11 @@
+<?php
+$host = 'localhost';
+$user = 'root';
+$password = '';
+$db = 'users';
+
+$conn = new mysqli ($host,$user,$password,$db);
+if(conn->connect_error) {
+	echo 'Sikeres kapcsolódás adatbázishoz!'}
+echo 'Sikeres db kapcsolódás!';
+?>
